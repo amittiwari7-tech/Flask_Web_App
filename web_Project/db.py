d38f27c3fe9d52f1de4bd0aa6ed5be711d37666b
Amit <amit.tiwari726@gmail.com>
2026-03-16 13:30:06 +0530

    changes

--- web_Project/db.py
+++ web_Project/db.py
@@ -1,16 +1,21 @@
+import os
+import pymysql
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
-import os
 
+# Render के लिए सबसे ज़रूरी फिक्स
+pymysql.install_as_MySQLdb()
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv() 
 
 mysql = MySQL()
+
 def init_db(app):
     app.config['MYSQL_HOST'] = os.getenv('dataBase_URL')
     app.config['MYSQL_USER'] = os.getenv('database_User')
     app.config['MYSQL_PASSWORD'] = os.getenv('database_Password')
     app.config['MYSQL_DB'] = os.getenv('database_Name')
-    app.config['MYSQL_PORT'] = int(os.getenv('database_Port',3306))
+    app.config['MYSQL_PORT'] = int(os.getenv('database_Port', 3306))
 
-    return mysql.init_app(app) 
+    mysql.init_app(app) # यह ऐप को कॉन्फ़िगर करता है
+    return mysql # यहाँ 'mysql' ऑब्जेक्ट रिटर्न करें
